e9f1e97acf6ff6734d86d252186f4138b5d6bdb3
gjparts <gjparts@gmail.com>
2024-05-31 08:58:52 -0600

    commit 0531202424337

---
+++ 00_primer_dia/src/pkg00_primer_dia/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg00_primer_dia;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //println imprime y luego salta al siguiente renglon
+        System.out.println("Hola UNAH");
+        System.out.println("Carrera de Sistemas 2024");
+        /*este es un comentario de
+        varios renglones
+        que se puede usar para tomar notas*/
+        System.out.print("Hola soy Gerardo");
+        System.out.print("Soy de SPS y aqui hace calor");
+        //solo hacer salto de renglon
+        System.out.println("");
+        System.out.println();
+        //tambien puede poner caracteres de escape
+        System.out.println("Esta es la\nclase de\nProgramacion 2");
+        System.out.print("\n\n\n\n\n\n\nHasta aqui\n");
+        System.out.println("Rojo\tAmarillo\tAzul\tVerde");
+        //si necesita imprimir slash inverso debe hacerlo asi:
+        System.out.println("Esto es un slash inverso \\");
+    }
+    
+}
