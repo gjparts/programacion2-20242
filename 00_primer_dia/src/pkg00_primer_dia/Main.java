f9127d2648d18fce821e38d5cb45c7a16ef5a538
gjparts <gjparts@gmail.com>
2024-06-04 11:09:02 -0600

    commit 0406202431549

--- 00_primer_dia/src/pkg00_primer_dia/Main.java
+++ 00_primer_dia/src/pkg00_primer_dia/Main.java
@@ -37,8 +37,8 @@ public class Main {
         int a;  //numero enteros
         float b; //real de presicion simple
         double c; //real de precision doble
-        char d; //almacena un caracter o sea 1 byte
-        boolean e; //almacena true o false o sea 1 bit
+        char dv; //almacena un caracter o sea 1 byte
+        boolean ev; //almacena true o false o sea 1 bit
         /*reglas para nombrar variables:
         1) no usar palabras reservadas (if, for, int, etc.)
         2) dos variable no se pueden llamar por el mismo nombre dentro del mismo ambito.
@@ -67,6 +67,55 @@ public class Main {
         System.out.println("valor de c2: "+c2);
         //System.out.println("valor de c2: "+c3); //JAVA no permite imprimir variables no inicializadas
         //System.out.println("valor de UNAH: "+UNAH);
+        int f = 1, e= 42;
+        double d = 8;
+        //concatenacion: el operador suma (+) en JAVA esta sobrecargado
+        //si ambos lados de la suma son numeros entonces se suman aritmeticamente
+        //si un lado de la suma es una cadena de texto entonces se unen o concatenan
+        //recuerde que en programacion toda expresion se evalua en pares de izquierda a derecha
+        //obedeciendo a la ley de precedencia de operadores
+        System.out.println("abc"+d+"xyz"+e+f);
+        System.out.println("abc"+d+"xyz"+(e+f));
+        
+        int entero1 = 7, entero2 = 9;
+        //lo siguiente devolvera un resultado incorrecto
+        System.out.println("La suma de "+entero1+" mas "+entero2+" es "+entero1+entero2);
+        //en estos casos lo anterior se arregla gracias a la ley de precedencia
+        //de operadores, aqui seria poner un parentesis para aislar la operacion aritmetica
+        System.out.println("La suma de "+entero1+" mas "+entero2+" es "+(entero1+entero2) );
+        
+        //uso de las funciones de la clase Math
+        System.out.println("El valor de PI es "+Math.PI);
+        //pow (elevar un valor base a determinada potencia)
+        //se recomienda usar double cuando trabaje con pow
+        double base = 3, exponente = 5;
+        System.out.println(base+" elevado a "+exponente+" es "+Math.pow(base,exponente));
+        double w = 2, t = 5, u;
+        u = Math.pow(2, 5);
+        System.out.println("el valor de u es "+u);
+        System.out.println(u+" algo");
+        System.out.println("algo "+u);
+        //en la siguiente expresion decimos que u es positivo
+        System.out.println(+u+" algo");
+        //en la siguiente expresion decimos que u es negativo
+        System.out.println(-u+" algo");
+        //lo siguiente da error ya que un String no tiene signo
+        //System.out.println(+"algo"+u);
+        //calculo de raiz cuadrada, se recomienda usar double
+        double cifra1 = 25;
+        System.out.println("La raiz cuadrada de "+cifra1+" es "+Math.sqrt(cifra1));
+        double cifra2 = 6, raiz2;
+        raiz2 = Math.sqrt(cifra2);
+        System.out.println("El valor de raiz2 es "+raiz2);
+        //con pow Usted puede obtener una raiz cubica:
+        double cifra8 = 27, cubica;
+        cubica = Math.pow(cifra8,1.0/3.0);
+        System.out.println("La raiz cubica de "+cifra8+" es "+cubica);
+        //importante, en JAVA no es lo mismo 1/3 que 1.0/3.0
+        //el resultado de 1/3 es 0 porque es una division de enteros
+        //el resultado de 1.0/3.0 es 0.333333 porque es una division de doubles
+        
+        
     }
     
 }
