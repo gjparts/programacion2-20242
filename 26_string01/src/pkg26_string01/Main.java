15e783cc92257dc137dabeb10ec1bca8ae435362
gjparts <gjparts@gmail.com>
2024-07-09 10:59:47 -0600

    commit 090720248912

--- 26_string01/src/pkg26_string01/Main.java
+++ 26_string01/src/pkg26_string01/Main.java
@@ -48,7 +48,58 @@ public class Main {
         System.out.println("Primer char de fruta2: "+fruta2.charAt(0));
         System.out.println("Ultimo char de fruta2: "+fruta2.charAt( fruta2.length()-1 ));
         
+        //obtener las veriones en mayusc./minusc. de un String
+        String comida1 = "Baleada con Todo";
+        //toUpperCase: este metodo devuelve una version en mayusculas
+        //de un String sin afectar a la variable original
+        System.out.println("comida1 en mayusculas: "+comida1.toUpperCase());
+        System.out.println("comida1: "+comida1);
+        //toLowerCase: este metodo devuelve una version en minusculas
+        //de un String sin afectar a la variable original
+        System.out.println("comida1 en mayusculas: "+comida1.toLowerCase());
+        System.out.println("comida1: "+comida1);
+        //afectar la variable original, o sea sobreescribir la variable
+        //por ejemplo: cambiar comida1 a mayusculas
+        comida1 = comida1.toUpperCase();
+        //de ahora en adelante comida1 queda afectada
+        System.out.println("comida1: "+comida1);
         
+        //obtener una version del string sin relleno de espacios iniciales y finales
+        //trim no afecta a la cadena original
+        String nombre123 = "     JOSUE   ";
+        System.out.println("nombre123: "+nombre123);
+        System.out.println("longitud de nombre123: "+nombre123.length());
+        //imprimir la version trim del String
+        System.out.println("trim de nombre123: "+nombre123.trim());
+        System.out.println("longitud de trim de nombre123: "+nombre123.trim().length()); 
+        //sobreescribir la varible original a su version trim
+        nombre123 = nombre123.trim();
+        System.out.println("nombre123: "+nombre123);
+        System.out.println("longitud de nombre123: "+nombre123.length());
+        
+        String verdura = "    chIlE dULcE             ";
+        //imprimir la variable verdura sin espacios de relleno, todo en mayusculas
+        //y sin alterar a la variable original; pero todo en una sola instruccion.
+        System.out.println( verdura.trim().toUpperCase() );
+        System.out.println( verdura.toUpperCase().trim() );
+        
+        //Concatenar elementos char a un String
+        String carrera = "Ingenieria en Sistemas";
+        //sin afectar a la var. original
+        System.out.println( carrera+'x' );
+        //afectar a la var. original
+        carrera = carrera + 'q';
+        System.out.println(carrera);
+        //concatenar varios char a un string
+        String nombre = "Gerardo";
+        nombre = nombre + 'x' + 'y' + 'z';
+        System.out.println(nombre);
+        
+        //concatenar una variable char a una var. String
+        char punto = '.';
+        String apellido = "Portillo";
+        apellido += punto; //apellido = apellido + punto
+        System.out.println(apellido);
     }
     
 }
