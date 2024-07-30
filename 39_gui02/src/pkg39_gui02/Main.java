2a9099e25d3bbe225ff7f531918e85c5dd3b80d5
gjparts <gjparts@gmail.com>
2024-07-30 10:08:40 -0600

    commit 3007202425710

---
+++ 39_gui02/src/pkg39_gui02/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg39_gui02;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //inicializar el objeto xyz que es de clase PantallaPrincipal
+        PantallaPrincipal xyz = new PantallaPrincipal();
+        //dibujo en pantalla
+        xyz.setVisible(true);
+    }
+    
+}
