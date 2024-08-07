dad05e97a472ae68e6f6318b676ac86c0d4c0cb3
gjparts <gjparts@gmail.com>
2024-08-07 11:04:20 -0600

    commit 0708202442

---
+++ 44_gui07/src/pkg44_gui07/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg44_gui07;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulario f = new Formulario();
+        f.setVisible(true);
+    }
+    
+}
