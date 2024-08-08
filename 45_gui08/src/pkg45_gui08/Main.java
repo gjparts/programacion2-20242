38831806fa181b78505d7c83e200b363b6fc6b95
GJParts <39961515+gjparts@users.noreply.github.com>
2024-08-08 10:56:13 -0600

    commit 0808202418460

---
+++ 45_gui08/src/pkg45_gui08/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg45_gui08;
+
+/**
+ *
+ * @author Gerardo Portillo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana1 abc = new Ventana1();
+        abc.setVisible(true);
+    }
+    
+}
